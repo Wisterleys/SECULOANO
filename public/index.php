7870e275e5b8a53fee4e23de45b7d8c7adc43f2a
Wisterleys <wister.sousa2@gmail.com>
2022-05-07 13:30:16 -0300

    Configuração básica de ambiente concluída

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/images/account-history-line-art-vector-260nw-273860531.webp" type="image/x-icon">
+    <title>Função SeculoAno</title>
+</head>
+<body>
+    <h1>kd?</h1>
+</body>
+</html>
